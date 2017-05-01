3f6757e7c05b900d2471f721767f3edddbd72322
sheryldarroch <sheryld1028@gmail.com>
2017-05-01 15:59:58 +0000

    fixed code so lightboxes and it's buttons are working, but it always selects the first slide. The displayMovieLightbox and displayMusicLightbox functions are still not able to get the value of the data-index of the img

--- js/script.js
+++ js/script.js
@@ -9,54 +9,6 @@ let movieSearch7 = "http://www.omdbapi.com/?t=top+gun";
 let movieSearch8 = "http://www.omdbapi.com/?t=despicable+me";
 let movieSearch9 = "http://www.omdbapi.com/?t=finding+nemo";
 
-//create Music gallery and lightbox
-function displayMusic1(data) {
-  let musicHTML = '';
-  $.each( data.albums.items, (i, item)=>{
-    musicHTML += '<div class="column">';
-    musicHTML += '<img data-index="' + i + '" src="' + item.images[1].url + '" class="hover-shadow music-thumb">';
-    musicHTML += '</div>';
-  });
-  $('#photos-m').html(musicHTML);
-  $('#photos-m').css('display', 'flex');
-  
-  let lightboxHTML = '<span class="close-cursor">&times;</span>';
-  lightboxHTML += '<div class="lightbox-content">';
-  $.each( data.albums.items, (i, item)=>{
-    lightboxHTML += '<div class="music-slides">';
-    lightboxHTML += '<img src="' + item.images[0].url + '" style="width:100%">';
-    lightboxHTML += '<div class="caption>"';
-    lightboxHTML += '<p class="lightbox-name"><strong>Album Name:</strong> ' + item.name + '</p>';
-    lightboxHTML += '<a class="lightbox-url" href="' + item.external_urls.spotify +'">Listen to this Album in Spotify</a>';
-    lightboxHTML += '</div>';
-    lightboxHTML += '</div>';
-  });
-  lightboxHTML += '<a class="prev">&#10094;</a>';
-  lightboxHTML += '<a class="next">&#10095;</a>';
-  lightboxHTML += '</div>';      
-  lightboxHTML += '</div>';      
-  $('#lightbox-m').html(lightboxHTML);
-  displayMusicLightbox('.column');
-}
-
-//create Movie Slides    
-function movieSlides(obj) {
-    let lightboxHTML = '';
-    $.each(obj, (i, item)=>{
-    lightboxHTML += '<div class="movie-slides">';
-    lightboxHTML += '<img class="moviePoster" src="' + item.Poster + '">';
-    lightboxHTML += '<div class="movie-caption>"';
-    lightboxHTML += '<p class="movie-title"><strong>Movie Title:</strong> ' + item.Title + '</p>';
-    lightboxHTML += '<p class="movie-year"><strong>Year:</strong> ' + item.Year + '</p>';  
-    lightboxHTML += '<p class="movie-rated"><strong>Rated:</strong> ' + item.Rated + '</p>';
-    lightboxHTML += '<p class="movie-genre"><strong>Genre:</strong> ' + item.Genre + '</p>';  
-    lightboxHTML += '<p class="movie-plot"><strong>Plot:</strong> ' + item.Plot + '</p>';    
-    lightboxHTML += '</div>';
-    lightboxHTML += '</div>';
-    });  
-    $('#lightbox-content-b').html(lightboxHTML);
-}
-
 //Open Lightbox helper function  
 function openLightbox(e) {
   $(e).css('display', 'block');
@@ -78,7 +30,6 @@ function currentMusicSlide(n) {
 }
  
 let slideIndex = 1;
-
 //Show Music Slides Helper Function
 function showMusicSlides(n) {
     let slides = $('.music-slides');
@@ -88,6 +39,145 @@ function showMusicSlides(n) {
         $(e).css('display', 'none');
     });
     $(slides[slideIndex-1]).css('display', 'block');
+}
+      
+//Add click function and display lightbox and slides when album is clicked helper function
+function displayMusicLightbox(e) {
+  $(e).click(()=>{
+          let index = $(e).attr('data-index');
+          console.log(index);
+          openLightbox('#lightbox-m');
+          currentMusicSlide(index);
+  });
+} 
+
+//Add click function and close Music Lightbox when X is clicked helper function
+function closeMusicLightbox(e) {
+  $(e).click(()=>{
+      closeLightbox('#lightbox-m');
+  });
+}
+  
+// Add click function and Show previous Music slide when prev arrow clicked
+function prevMusicSlide(e) {
+  $(e).click (()=>{
+      plusMusicSlides(-1);
+  });
+}
+    
+// Add click function and Show next music slide when next arrow clicked
+function nextMusicSlide(e) {
+    $(e).click (()=>{
+    plusMusicSlides(1);
+  }); 
+}
+
+//create Music gallery and lightbox
+function displayMusic1(data) {
+  let musicHTML = '';
+  $.each( data.albums.items, (i, item)=>{
+    musicHTML += '<div class="column">';
+    musicHTML += '<img data-index="' + i + '" src="' + item.images[1].url + '" class="hover-shadow music-thumb">';
+    musicHTML += '</div>';
+  });
+  $('#photos-m').html(musicHTML);
+  $('#photos-m').css('display', 'flex');
+  
+  let lightboxHTML = '<span class="close-cursor">&times;</span>';
+  lightboxHTML += '<div class="lightbox-content">';
+  $.each( data.albums.items, (i, item)=>{
+    lightboxHTML += '<div class="music-slides">';
+    lightboxHTML += '<img src="' + item.images[0].url + '" style="width:100%">';
+    lightboxHTML += '<div class="caption">';
+    lightboxHTML += '<p class="lightbox-name"><strong>Album Name:</strong> ' + item.name + '</p>';
+    lightboxHTML += '<a class="lightbox-url" href="' + item.external_urls.spotify +'">Listen to this Album in Spotify</a>';
+    lightboxHTML += '</div>';
+    lightboxHTML += '</div>';
+  });
+  lightboxHTML += '<a class="prev">&#10094;</a>';
+  lightboxHTML += '<a class="next">&#10095;</a>';
+  lightboxHTML += '</div>';      
+  lightboxHTML += '</div>';      
+  $('#lightbox-m').html(lightboxHTML);
+  //show lightbox when album is clicked  
+  displayMusicLightbox('.music-thumb');
+  //close lightbox when x is clicked    
+  closeMusicLightbox('.close-cursor');
+  //show previous slide when prev arrow is clicked    
+  prevMusicSlide('.prev');
+  //show next slide when next arrow is clicked    
+  nextMusicSlide('.next');
+}
+      
+//Show Prev/Next Movie Slide Helper Function    
+function plusMovieSlides(n) {
+    showMovieSlides(movieIndex += n);
+}
+
+//Show Current Movie Slide Helper Function    
+function currentMovieSlide(n) {
+    showMovieSlides(movieIndex = n + 1);
+}
+ 
+let movieIndex = 1;
+//Show Movie Slides Helper Function
+function showMovieSlides(n) {
+    let slides = $('.movie-slides');
+    if (n > slides.length) {movieIndex = 1};
+    if (n < 1) {movieIndex = slides.length};
+    $(slides).each((i, e)=>{
+        $(e).css('display', 'none');
+    });
+    $(slides[movieIndex-1]).css('display', 'block');
+}  
+  
+//add click function and Display lightbox and slides when movie poster is clicked
+function displayMovieLightbox(e) {      
+  $(e).click(()=>{
+          let index = $(e).attr("data-index");
+          console.log(index);
+          openLightbox('#lightbox-b');
+          currentMovieSlide(index);
+  });
+}  
+      
+//add click function and Close lightbox when "X" is clicked
+function closeMovieLightbox(e) {
+   $(e).click(()=>{
+   closeLightbox('#lightbox-b');
+   });
+}  
+      
+//add click function and Show previous slide when prev arrow clicked
+function prevMovieSlide(e) {      
+  $(e).click (()=>{
+      plusMovieSlides(-1);
+  });
+}
+      
+//add click function and Show next slide when next arrow clicked
+function nextMovieSlide(e) {      
+  $(e).click (()=>{
+      plusMovieSlides(1);
+  }); 
+}  
+
+//create Movie Slides    
+function movieSlides(obj) {
+    let lightboxHTML = '';
+    $.each(obj, (i, item)=>{
+    lightboxHTML += '<div class="movie-slides">';
+    lightboxHTML += '<img class="moviePoster" src="' + item.Poster + '">';
+    lightboxHTML += '<div class="movie-caption">';
+    lightboxHTML += '<p class="movie-title"><strong>Movie Title:</strong> ' + item.Title + '</p>';
+    lightboxHTML += '<p class="movie-year"><strong>Year:</strong> ' + item.Year + '</p>';  
+    lightboxHTML += '<p class="movie-rated"><strong>Rated:</strong> ' + item.Rated + '</p>';
+    lightboxHTML += '<p class="movie-genre"><strong>Genre:</strong> ' + item.Genre + '</p>';  
+    lightboxHTML += '<p class="movie-plot"><strong>Plot:</strong> ' + item.Plot + '</p>';    
+    lightboxHTML += '</div>';
+    lightboxHTML += '</div>';
+    });  
+    $('#lightbox-content-b').html(lightboxHTML);
 }
 
 // Display album selections when button is clicked    
@@ -127,30 +217,6 @@ $('#btn-m-3').click(()=>{
   $.getJSON(spotifyAPI, spotifyOptions, displayMusic1);
 });
 
-//Display lightbox and slides when album is clicked helper function
-function displayMusicLightbox(e) {
-  $(e).click((event)=>{
-          let img = $(event.target).children();
-          let index = img.data(dataindex);
-          console.log(index);
-          openLightbox('#lightbox-m');
-          currentMusicSlide(index);
-  });
-}  
-//Close lightbox when "X" is clicked
-$('.close-cursor').click(()=>{
-    closeLightbox('#lightbox-m');
-});
-  
-// Show previous Music slide when prev arrow clicked
-$('.prev').click (()=>{
-    plusMusicSlides(-1);
-});
-  
-// Show next music slide when next arrow clicked
-$('.next').click (()=>{
-    plusMusicSlides(1);
-}); 
 
 //Call Movie AJAX Requests and Create Movie Lightbox
 $.when($.getJSON(movieSearch0),
@@ -168,52 +234,9 @@ $.when($.getJSON(movieSearch0),
           let searches = [];
           searches.push(data0[0], data1[0], data2[0], data3[0], data4[0], data5[0], data6[0], data7[0], data8[0], data9[0]);
           movieSlides(searches);
+          displayMovieLightbox('.movies');
+          closeMovieLightbox('.movie-close-cursor');
+          prevMovieSlide('.movie-prev');
+          nextMovieSlide('.movei-next');     
     });
 
-//Show Prev/Next Movie Slide Helper Function    
-function plusMovieSlides(n) {
-    showMovieSlides(movieIndex += n);
-}
-
-//Show Current Movie Slide Helper Function    
-function currentMovieSlide(n) {
-    showMovieSlides(movieIndex = n + 1);
-}
- 
-let movieIndex = 1;
-
-//Show Movie Slides Helper Function
-function showMovieSlides(n) {
-    let slides = $('.movie-slides');
-    if (n > slides.length) {movieIndex = 1};
-    if (n < 1) {movieIndex = slides.length};
-    $(slides).each((i, e)=>{
-        $(e).css('display', 'none');
-    });
-    $(slides[movieIndex-1]).css('display', 'block');
-}  
-  
-//Display lightbox and slides when movie poster is clicked
-$('.movie-thumb').click((event)=>{
-        let img = $(event.target).children();
-        let index = img.data(dataindex);
-        console.log(index);
-        openLightbox('#lightbox-b');
-        currentMovieSlide(index);
-});
-  
-//Close lightbox when "X" is clicked
-$('.movie-close-cursor').click(()=>{
-    closeLightbox('#lightbox-b');
-});
-  
-// Show previous slide when prev arrow clicked
-$('.movie-prev').click (()=>{
-    plusMovieSlides(-1);
-});
-  
-// Show next slide when next arrow clicked
-$('.movie-next').click (()=>{
-    plusMovieSlides(1);
-}); 
-  

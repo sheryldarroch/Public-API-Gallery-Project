a43bbd2462cb165ce2cbc6f5e6bcebb74515f6b4
sheryldarroch <sheryld1028@gmail.com>
2017-05-04 17:36:36 +0000

    finally figured out the code for the lightboxes and slides

--- js/script.js
+++ js/script.js
@@ -9,141 +9,96 @@ let movieSearch7 = "http://www.omdbapi.com/?t=top+gun";
 let movieSearch8 = "http://www.omdbapi.com/?t=despicable+me";
 let movieSearch9 = "http://www.omdbapi.com/?t=finding+nemo";
 
-let slideIndex = 1;
+//Music Slides Helper Function to use with Click Arrows
 function showMusicSlides(n) {
-    let slides = $('.music-slides');
-    if (n > slides.length) {slideIndex = 1};
-    if (n < 1) {slideIndex = slides.length};
-    $(slides).each((i, e)=>{
-        $(e).css('display', 'none');
-    });
-    $(slides[slideIndex]).css('display', 'block');
+  let musicSlides = $('.music-slides');
+  let currentSlide = $('.music-slides:visible');
+  let index = musicSlides.index(currentSlide);
+  index += n;
+  if(index < 0) {
+    index = musicSlides.length -1;
+  } else if(index >= musicSlides.length) {
+      index = 0;
+    }
+    musicSlides.hide();
+    musicSlides.eq(index).show();
 }
-      
-//let movieIndex = 1;
-////Show Movie Slides Helper Function
-//function showMovieSlides(n) {
-//    let movieIndex = (+n) - 1;
-//    console.log(movieIndex);
-//    let slides = $('.movie-slides');
-//    console.log(slides.length);
-//    if(n > slides.length) {movieIndex = 0};
-//    if(n < 0) {movieIndex = slides.length};
-//    console.log(n);
-//    $(slides).each((i, e)=>{
-//        $(e).css('display', 'none');
-//    });
-//    $(slides[movieIndex]).css('display', 'block');
-//}  
 
-let movieIndex = 0;
+//Movie Slides Helper Function to use with Click Arrows
 function showMovieSlides(n) {
-      let slides = $('.movie-slides');
-//      movieIndex = n;
-//      console.log(movieIndex);
-      if (n >= slides.length) {
-        movieIndex = 0;
-      }
-      if (n < 0) {
-        movieIndex = slides.length - 1;
+      let movieSlides = $('.movie-slides');
+      let currentSlide = $('.movie-slides:visible');
+      let index = movieSlides.index(currentSlide);
+      index += n;
+      if(index < 0) {
+          index = movieSlides.length -1;
+       } else if(index >= movieSlides.length) {
+            index = 0;
+         }
+         movieSlides.hide();
+         movieSlides.eq(index).show();
+}
+
+//Add click function and move through Music Slides when arrows are clicked
+function clickMusicArrow(e) {
+    $(e).click((event)=>{
+      let target = $(event.target);
+      if(target.is('.next')) {
+          showMusicSlides(1);
       } else {
-        movieIndex = n;
-      }
-      slides.each((i,e)=>{
-          $(e).css('display', 'none');
-      });
-      slides.eq(movieIndex).css('display', 'block'); 
-      console.log(movieIndex);
-}      
-      
-      
-// Add click function and Show previous Music slide when prev arrow clicked
-function prevMusicSlide(e) {
-  $(e).click (()=>{
-      slideIndex += -1;
-      showMusicSlides(slideIndex);
-  });
+            showMusicSlides(-1);       
+        }
+    });
 }
+
+//Add click function and move through Movie Slides when arrows are clicked
+function clickMovieArrow(e) {
+    $(e).click((event)=>{
+      let target = $(event.target);
+      if(target.is('.movie-next')) {
+          showMovieSlides(1);
+      } else {
+            showMovieSlides(-1);
+        }
+    });
+}
+      
     
-// Add click function and Show next music slide when next arrow clicked
-function nextMusicSlide(e) {
-    $(e).click (()=>{
-      slideIndex += 1;
-      showMusicSlides(slideIndex);
-  }); 
-}
-      
-//add click function and Show previous slide when prev arrow clicked
-function prevMovieSlide(e) {      
-  $(e).click (()=>{
-      console.log(movieIndex)
-      movieIndex += -1;
-      console.log(movieIndex);
-      showMovieSlides(movieIndex);
-  });
-}
-      
-//add click function and Show next slide when next arrow clicked
-function nextMovieSlide(e) {      
-  $(e).click (()=>{
-      console.log(movieIndex);
-      movieIndex += 1;
-      console.log(movieIndex);
-      showMovieSlides(movieIndex);      
-  }); 
-}        
-      
 //Add click function and display music lightbox and slides when album is clicked
 function displayMusicLightbox(e) {
   $(e).click((event)=>{
-          let index = $(event.target).attr('data-index');
-          $('#lightbox-m').css('display', 'block');
-          showMusicSlide(index);
+          let albumIndex = $(event.target).attr('data-index');
+          let index = parseInt(albumIndex, 10);
+          let musicSlides = $('.music-slides');
+          $('#lightbox-m').show();
+          musicSlides.eq(index).show();          
   });
 } 
 
 //add click function and display movie lightbox and slides when movie poster is clicked
 function displayMovieLightbox(e) {      
   $(e).click((event)=>{
-          let index = $(event.target).attr("data-index");
-          parseInt(index, 10);
-          console.log(index);
-          $('#lightbox-b').css('display', 'block');
-          showMovieSlides(index);
-//          prevMovieSlide('.movie-prev');
-//          nextMovieSlide('.movie-next');
-//          //show previous slide when prev arrow is clicked
-//          $('.movie-prev').click (()=>{ 
-//              movieIndex = index - 1;
-//              console.log(movieIndex);
-//              showMovieSlides(movieIndex);
-//          }); 
-//          $('.movie-next').click (()=>{
-//              movieIndex = index + 1;
-//              console.log(index);      
-//              showMovieSlides(movieIndex);      
-//          }); 
+          let posterIndex = $(event.target).attr("data-index");
+          let index = parseInt(posterIndex, 10);
+          let movieSlides = $('.movie-slides');
+          $('#lightbox-b').show();
+           movieSlides.eq(index).show();
   });
-
-     
 }  
 
-//Add click function and close Music Lightbox when X is clicked helper function
-function closeMusicLightbox(e) {
+//Add click function and close Lightbox when X is clicked helper function
+function closeLightbox(e) {
   $(e).click(()=>{
+    if (e === '.close-cursor') {
       $('#lightbox-m').css('display', 'none');
+    } else if (e === '.movie-close-cursor') {
+      $('#lightbox-b').css('display', 'none'); 
+    }
   });
 }
 
-//add click function and Close Movie lightbox when "X" is clicked
-function closeMovieLightbox(e) {
-   $(e).click(()=>{
-      $('#lightbox-b').css('display', 'none');    
-   });
-}  
-
 //create Music gallery and lightbox
-function displayMusic1(data) {
+function displayMusic(data) {
   let musicHTML = '';
   $.each( data.albums.items, (i, item)=>{
     musicHTML += '<div class="column">';
@@ -164,23 +119,18 @@ function displayMusic1(data) {
     lightboxHTML += '</div>';
     lightboxHTML += '</div>';
   });
-  lightboxHTML += '<a class="prev">&#10094;</a>';
-  lightboxHTML += '<a class="next">&#10095;</a>';
+  lightboxHTML += '<a class="arrow prev">&#10094;</a>';
+  lightboxHTML += '<a class="arrow next">&#10095;</a>';
   lightboxHTML += '</div>';      
   lightboxHTML += '</div>';      
   $('#lightbox-m').html(lightboxHTML);
-  //show lightbox when album is clicked  
   displayMusicLightbox('.music-thumb');
-  //close lightbox when x is clicked    
-  closeMusicLightbox('.close-cursor');
-  //show previous slide when prev arrow is clicked       
-  prevMusicSlide('.prev');
-  //show next slide when next arrow is clicked    
-  nextMusicSlide('.next');      
+  closeLightbox('.close-cursor');
+  clickMusicArrow('.arrow');  
 }
 
 //create Movie Slides    
-function movieSlides(obj) {
+function createMovieSlides(obj) {
     let lightboxHTML = '';
     $.each(obj, (i, item)=>{
     lightboxHTML += '<div class="movie-slides">';
@@ -207,7 +157,7 @@ $('#btn-m-1').click(()=>{
       type: "album",
       limit: 10
   };  
-  $.getJSON(spotifyAPI, spotifyOptions, displayMusic1);
+  $.getJSON(spotifyAPI, spotifyOptions, displayMusic);
 });
 
 $('#btn-m-2').click(()=>{
@@ -219,7 +169,7 @@ $('#btn-m-2').click(()=>{
       type: "album",
       limit: 10
   };  
-  $.getJSON(spotifyAPI, spotifyOptions, displayMusic1);
+  $.getJSON(spotifyAPI, spotifyOptions, displayMusic);
 });
 
 $('#btn-m-3').click(()=>{
@@ -231,7 +181,7 @@ $('#btn-m-3').click(()=>{
       type: "album",
       limit: 10
   };  
-  $.getJSON(spotifyAPI, spotifyOptions, displayMusic1);
+  $.getJSON(spotifyAPI, spotifyOptions, displayMusic);
 });
 
 
@@ -250,10 +200,9 @@ $.when($.getJSON(movieSearch0),
       .done(function(data0, data1, data2, data3, data4, data5, data6, data7, data8, data9) {
           let searches = [];
           searches.push(data0[0], data1[0], data2[0], data3[0], data4[0], data5[0], data6[0], data7[0], data8[0], data9[0]);
-          movieSlides(searches);
+          createMovieSlides(searches);
           displayMovieLightbox('.movies');
-          closeMovieLightbox('.movie-close-cursor');
-          prevMovieSlide('.movie-prev');
-          nextMovieSlide('.movie-next');     
+          closeLightbox('.movie-close-cursor');
+          clickMovieArrow('.movie-arrow');
     });
 
